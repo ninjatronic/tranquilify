4526805247f1dad8429bc96a3a110fbc56312007
Pete Martin <pete@ninjatronic.com>
2013-11-23 11:22:11 +0000

    ui keeping state of play/pause/working - using promises

--- www/js/control/main.js
+++ www/js/control/main.js
@@ -9,6 +9,9 @@
 
                 $scope.$state = $state;
 
+                $scope.working = false;
+                $scope.playing = false;
+
                 function loadAudio(id) {
                     var deferred = $q.defer();
                     LowLatencyAudio.preloadAudio(id, 'media/'+id+'.mp3', 2,
@@ -24,18 +27,54 @@
                     return deferred.promise;
                 }
 
+                function loopAudio(id) {
+                    var deferred = $q.defer();
+                    LowLatencyAudio.loop(id,
+                        function() {
+                            $scope.$apply(function() {
+                                deferred.resolve(id);
+                            });
+                        }, function() {
+                            $scope.$apply(function() {
+                                deferred.reject();
+                            });
+                        });
+                    return deferred.promise;
+                }
+
+                function stopAudio(id) {
+                    var deferred = $q.defer();
+                    LowLatencyAudio.stop(id,
+                        function() {
+                            $scope.$apply(function() {
+                                deferred.resolve(id);
+                            });
+                        }, function() {
+                            $scope.$apply(function() {
+                                deferred.reject();
+                            });
+                        });
+                    return deferred.promise;
+                }
+
                 var loader = loadAudio('ocean');
 
                 $scope.play = function() {
-                    $scope.playing = true;
+                    $scope.working = true;
                     loader.then(function() {
-                        LowLatencyAudio.loop('ocean');
+                        loopAudio('ocean').then(function() {
+                            $scope.playing = true;
+                            $scope.working = false;
+                        });
                     });
                 };
 
                 $scope.pause = function() {
-                    delete $scope.playing;
-                    LowLatencyAudio.stop('ocean');
+                    $scope.working = true;
+                    stopAudio('ocean').then(function() {
+                        $scope.playing = false;
+                        $scope.working = false;
+                    });
                 };
 
             }]);
